b2306d106e3105bb6130934909e88abcb9bde225
fikra arifin <fkhrrfn@gmail.com>
2026-08-16 06:42:03 +0700

    fix: merge mobile_interactions into app.js to bypass vite manifest errors

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/mobile_interactions.js' // <-- TAMBAHKAN BARIS INI
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
