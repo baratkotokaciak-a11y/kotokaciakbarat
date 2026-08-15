11ddf8e4fe36921c217f2a77317c8becc293162c
fikra arifin <fkhrrfn@gmail.com>
2026-08-16 05:18:02 +0700

    Update konfigurasi aplikasi

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Mengarahkan folder storage & bootstrap cache ke direktori temporary (/tmp)
+// 1. Buat folder temporary di /tmp
 $storageFolders = [
     '/tmp/storage/app/public',
     '/tmp/storage/framework/views',
@@ -16,8 +16,21 @@ foreach ($storageFolders as $folder) {
     }
 }
 
-// Bind path storage baru sebelum Laravel booting
-putenv('APP_STORAGE_PATH=/tmp/storage');
-$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
+// 2. Set Environment variable untuk lokasi compiled views & bootstrap cache
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
+
+// 3. Load aplikasi Laravel bawaan
 require __DIR__ . '/../public/index.php';
