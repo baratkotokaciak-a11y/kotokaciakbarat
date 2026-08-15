e53bf02a3b3f146e33e2b1748e85152f0bc1e172
fikra arifin <fkhrrfn@gmail.com>
2026-08-16 05:06:40 +0700

    Fix Vercel outputDirectory setting

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 
+// Mengarahkan folder storage & bootstrap cache ke direktori temporary (/tmp)
+$storageFolders = [
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
+}
+
+// Bind path storage baru sebelum Laravel booting
+putenv('APP_STORAGE_PATH=/tmp/storage');
+$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
+
 require __DIR__ . '/../public/index.php';
